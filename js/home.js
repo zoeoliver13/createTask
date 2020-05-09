9f6387386f2285041ea5b09e81731fa4820dba33
Bryan Oliver <cookieez@Bryans-Air.fios-router.home>
2020-05-08 22:05:54 -0400

    started

---
+++ js/home.js
@@ -0,0 +1,3 @@
+function popUp(){
+  alert("The calculations of unweighted GPAs are way simpler than weighted GPAs. An A is a 4.0, despite the difficulty of the class. However, A weighted GPA depends of the course load and are sometimes measured up to 5.0 points.")
+}

81c5964d7e4369a24a8fe88fc66e7e4dfea660c3
Bryan Oliver <cookieez@Bryans-Air.fios-router.home>
2020-05-09 23:17:59 -0400

    ore stuff

--- js/wgpa.js
+++ js/wgpa.js
@@ -13,7 +13,10 @@ function addClass(table) {
   grades.setAttribute("type","text");
   grades.setAttribute("size", "2");
   grades.placeholder = "A";
-  grades.style.margin = "30px 10px 10px 10px";
+  grades.style.margin = "30px 10px 10px 25px";
+  grades.style.padding = "10px 10px 10px 10px";
+  grades.style.textAlign = "center";
+  grades.style.borderRadius = "5px";
   cell2.appendChild(grades);
   var cell3 = row.insertCell(2);
   var weight = document.createElement("INPUT");
@@ -21,13 +24,23 @@ function addClass(table) {
   weight.setAttribute("size", "20");
   weight.style.margin = "30px 10px 10px 10px";
   weight.placeholder = "Regular";
+  weight.style.padding = "10px 10px 10px 10px";
+  weight.style.textAlign = "center";
+  weight.style.borderRadius = "5px";
   cell3.appendChild(weight);
   /*DELETE BUTTON*/
   var cell4 = row.insertCell(3);
   var img = document.createElement("img");
   img.src = "garbage.jpg";
   img.width = "50";
+  img.className = "close"
   cell4.appendChild(img);
   /*REMOVING A ROW*/
-
+  var close = document.getElementsByClassName("close");
+      for (var i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+    var div = this.parentElement.parentElement;
+    div.remove();
+          }
+        }
 }

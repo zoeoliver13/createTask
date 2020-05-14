38ec6eeb4c416022a1efff802f8e3bbec7762fb4
Daneliz Urena <durena402@gmail.com>
2020-05-14 18:10:35 -0400

    almost done

--- js/uwgpa.js
+++ js/uwgpa.js
@@ -1,5 +1,6 @@
 const grade = [];
 const credit = [];
+const weight = [];
 
 var grade1;
 var grade2;
@@ -20,67 +21,72 @@ var credit8;
 var average = 0;
 
 function submit(){
-  grade1 = document.getElementById('grade1');
+  grade1 = document.getElementById('grade1').value;
   grade.push(grade1);
-  console.log(grade1);
 
-  grade2 = document.getElementById('grade2');
+
+  grade2 = document.getElementById('grade2').value;
   grade.push(grade2);
 
-  grade3 = document.getElementById('grade3');
+  grade3 = document.getElementById('grade3').value;
+  grade.push(grade3);
+
+  grade4 = document.getElementById('grade4').value;
   grade.push(grade4);
 
-  grade4 = document.getElementById('grade4');
-  grade.push(grade4);
-
-  grade5 = document.getElementById('grade5');
+  grade5 = document.getElementById('grade5').value;
   grade.push(grade5);
 
-  grade6 = document.getElementById('grade6');
+  grade6 = document.getElementById('grade6').value;
   grade.push(grade6);
 
-  grade7 = document.getElementById('grade7');
+  grade7 = document.getElementById('grade7').value;
   grade.push(grade7);
 
-  grade8 = document.getElementById('grade8');
+  grade8 = document.getElementById('grade8').value;
   grade.push(grade8);
 
-  credit1 = document.getElementById('credit1');
+  credit1 = document.getElementById('credit1').value;
   credit.push(credit1);
 
-  credit2 = document.getElementById('credit2');
+  credit2 = document.getElementById('credit2').value;
   credit.push(credit2);
 
-  credit3 = document.getElementById('credit3');
+  credit3 = document.getElementById('credit3').value;
   credit.push(credit3);
 
-  credit4 = document.getElementById('credit4');
+  credit4 = document.getElementById('credit4').value;
   credit.push(credit4);
 
-  credit5 = document.getElementById('credit5');
+  credit5 = document.getElementById('credit5').value;
   credit.push(credit5);
 
-  credit6 = document.getElementById('credit6');
+  credit6 = document.getElementById('credit6').value;
   credit.push(credit6);
 
-  credit7 = document.getElementById('credit7');
+  credit7 = document.getElementById('credit7').value;
   credit.push(credit7);
 
-  credit8 = document.getElementById('credit8');
+  credit8 = document.getElementById('credit8').value;
   credit.push(credit8);
 
   calculateGpa();
+
+
+
 };
 
 function calculateGpa(){
 let sum = 0;
 let sumCredit = 0;
   for (let i = 0; i < 8; i++) {
+    credit[i] = parseInt(credit[i]);
       sum += grade[i] * credit[i];
   }
   for (let y = 0; y < 8; y++){
       sumCredit += credit[y];
   }
+
   average = sum/sumCredit;
   console.log(average);
 }
--- js/wgpa.js
+++ js/wgpa.js
@@ -29,83 +29,82 @@ var weight8;
 var average = 0;
 
 function submit(){
-  grade1 = document.getElementById('grade1');
+  grade1 = document.getElementById('grade1').value;
   grade.push(grade1);
-  console.log(grade1);
 
-  grade2 = document.getElementById('grade2');
+
+  grade2 = document.getElementById('grade2').value;
   grade.push(grade2);
 
-  grade3 = document.getElementById('grade3');
+  grade3 = document.getElementById('grade3').value;
+  grade.push(grade3);
+
+  grade4 = document.getElementById('grade4').value;
   grade.push(grade4);
 
-  grade4 = document.getElementById('grade4');
-  grade.push(grade4);
-
-  grade5 = document.getElementById('grade5');
+  grade5 = document.getElementById('grade5').value;
   grade.push(grade5);
 
-  grade6 = document.getElementById('grade6');
+  grade6 = document.getElementById('grade6').value;
   grade.push(grade6);
 
-  grade7 = document.getElementById('grade7');
+  grade7 = document.getElementById('grade7').value;
   grade.push(grade7);
 
-  grade8 = document.getElementById('grade8');
+  grade8 = document.getElementById('grade8').value;
   grade.push(grade8);
 
-  credit1 = document.getElementById('credit1');
+  credit1 = document.getElementById('credit1').value;
   credit.push(credit1);
 
-  credit2 = document.getElementById('credit2');
+  credit2 = document.getElementById('credit2').value;
   credit.push(credit2);
 
-  credit3 = document.getElementById('credit3');
+  credit3 = document.getElementById('credit3').value;
   credit.push(credit3);
 
-  credit4 = document.getElementById('credit4');
+  credit4 = document.getElementById('credit4').value;
   credit.push(credit4);
 
-  credit5 = document.getElementById('credit5');
+  credit5 = document.getElementById('credit5').value;
   credit.push(credit5);
 
-  credit6 = document.getElementById('credit6');
+  credit6 = document.getElementById('credit6').value;
   credit.push(credit6);
 
-  credit7 = document.getElementById('credit7');
+  credit7 = document.getElementById('credit7').value;
   credit.push(credit7);
 
-  credit8 = document.getElementById('credit8');
+  credit8 = document.getElementById('credit8').value;
   credit.push(credit8);
 
-  weight1 = document.getElementById('weight1');
+  weight1 = document.getElementById('weight1').value;
   weight.push(weight1);
 
-  weight2 = document.getElementById('weight2');
+  weight2 = document.getElementById('weight2').value;
   weight.push(weight2);
 
-  weight3 = document.getElementById('weight3');
+  weight3 = document.getElementById('weight3').value;
   weight.push(weight3);
 
-  weight4 = document.getElementById('weight4');
+  weight4 = document.getElementById('weight4').value;
   weight.push(weight4);
 
-  weight5 = document.getElementById('weight5');
+  weight5 = document.getElementById('weight5').value;
   weight.push(weight5);
 
-  weight6 = document.getElementById('weight6');
+  weight6 = document.getElementById('weight6').value;
   weight.push(weight6);
 
-  weight7 = document.getElementById('weight7');
+  weight7 = document.getElementById('weight7').value;
   weight.push(weight7);
 
-  weight8 = document.getElementById('weight8');
+  weight8 = document.getElementById('weight8').value;
   weight.push(weight8);
 
-  console.log(credit1);
   calculateGpa();
 
-  
+
 
 };
 
@@ -113,11 +112,13 @@ function calculateGpa(){
 let sum = 0;
 let sumCredit = 0;
   for (let i = 0; i < 8; i++) {
+    credit[i] = parseInt(credit[i]);
       sum += grade[i] * credit[i] * weight[i];
   }
   for (let y = 0; y < 8; y++){
       sumCredit += credit[y];
   }
+
   average = sum/sumCredit;
   console.log(average);
 }

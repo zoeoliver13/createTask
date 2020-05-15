6fdbf63683d6aaeb9be4d69bf9add79b4d9c0981
Bryan Oliver <cookieez@Bryans-Air.fios-router.home>
2020-05-15 17:38:43 -0400

    done?

--- js/uwgpa.js
+++ js/uwgpa.js
@@ -1,6 +1,4 @@
-const grade = [];
-const credit = [];
-
+//VARIABLES//
 var grade1;
 var grade2;
 var grade3;
@@ -19,79 +17,72 @@ var credit7;
 var credit8;
 var average = 0;
 
-function submit(){
-  grade1 = document.getElementById('grade1').value;
-  grade.push(grade1);
-
-
-  grade2 = document.getElementById('grade2').value;
-  grade.push(grade2);
-
-  grade3 = document.getElementById('grade3').value;
-  grade.push(grade3);
-
-  grade4 = document.getElementById('grade4').value;
-  grade.push(grade4);
-
-  grade5 = document.getElementById('grade5').value;
-  grade.push(grade5);
-
-  grade6 = document.getElementById('grade6').value;
-  grade.push(grade6);
-
-  grade7 = document.getElementById('grade7').value;
-  grade.push(grade7);
-
-  grade8 = document.getElementById('grade8').value;
-  grade.push(grade8);
-
-  credit1 = document.getElementById('credit1').value;
-  credit.push(credit1);
-
-  credit2 = document.getElementById('credit2').value;
-  credit.push(credit2);
-
-  credit3 = document.getElementById('credit3').value;
-  credit.push(credit3);
-
-  credit4 = document.getElementById('credit4').value;
-  credit.push(credit4);
-
-  credit5 = document.getElementById('credit5').value;
-  credit.push(credit5);
-
-  credit6 = document.getElementById('credit6').value;
-  credit.push(credit6);
-
-  credit7 = document.getElementById('credit7').value;
-  credit.push(credit7);
-
-  credit8 = document.getElementById('credit8').value;
-  credit.push(credit8);
-
-  calculateGpa();
-
-
-
-};
-
-function calculateGpa(){
-let sum = 0;
-let sumCredit = 0;
-let gpa = document.getElementById("gpa");
-  for (let i = 0; i < 8; i++) {
-    credit[i] = parseInt(credit[i]);
-      sum += grade[i] * credit[i];
+//FUNCTIONS//
+  //Calculates GPA//
+function getGPA(){
+var sum = 0;
+var credits = 0;
+var gpa = document.getElementById("gpa");
+  for (var x = 0; x < 8; x++) {
+    credit[x] = parseInt(credit[x]);
+      sum += grade[x] * credit[x];
   }
-  for (let y = 0; y < 8; y++){
-      sumCredit += credit[y];
+  for (var y = 0; y < 8; y++){
+      credits += credit[y];
   }
 
-  average = sum/sumCredit;
-  average = average.toLocaleString ('en', {minimumFractionDigits : 3 , maximimFractionDigits: 3});
+  average = sum/credits;
+  //Prints under the button//
+  average = average.toLocaleString ('en', {minimumFractionDigits : 1 , maximimFractionDigits: 1});
   gpa.innerHTML = average;
 }
- //RELOAD THE PAGE
+const grade = [];
+const credit = [];
+
+
+//Assigning variables to all spots in each row//
+function submit(){
+  grade1 = document.getElementById('grade1').value;
+    grade.push(grade1);
+  grade2 = document.getElementById('grade2').value;
+    grade.push(grade2);
+  grade3 = document.getElementById('grade3').value;
+    grade.push(grade3);
+  grade4 = document.getElementById('grade4').value;
+    grade.push(grade4);
+  grade5 = document.getElementById('grade5').value;
+    grade.push(grade5);
+  grade6 = document.getElementById('grade6').value;
+    grade.push(grade6);
+  grade7 = document.getElementById('grade7').value;
+    grade.push(grade7);
+  grade8 = document.getElementById('grade8').value;
+    grade.push(grade8);
+  credit1 = document.getElementById('credit1').value;
+    credit.push(credit1);
+  credit2 = document.getElementById('credit2').value;
+    credit.push(credit2);
+  credit3 = document.getElementById('credit3').value;
+    credit.push(credit3);
+  credit4 = document.getElementById('credit4').value;
+    credit.push(credit4);
+  credit5 = document.getElementById('credit5').value;
+    credit.push(credit5);
+  credit6 = document.getElementById('credit6').value;
+    credit.push(credit6);
+  credit7 = document.getElementById('credit7').value;
+    credit.push(credit7);
+  credit8 = document.getElementById('credit8').value;
+    credit.push(credit8);
+
+
+  getGPA();
+};
+
+ //RELOAD THE PAGE//
  function reload(){
-   window.location.reload();
+   var yes = confirm("Are you sure you want to clear your inputs to make a new one? ");
+   if(yes == true){
+     window.location.reload();
+   }
  }
